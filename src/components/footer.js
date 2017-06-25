533584cce8cd353984da5e40d98bd1812c62dbd4
Jacky Lee <aboutjax1990@gmail.com>
2017-06-25 17:02:20 +1200

    update image

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,7 +14,7 @@ function Footer() {
 
         <img className="c-footer__power-by-strava" src={ assetSrc + "/img/api_logo_pwrdBy_strava_horiz_white.svg"}/>
         <span>
-          Made with ☕️ by Jacky Lee
+          Made with ☕️ by <a className="c-link" href="https://jackylee.co">Jacky Lee</a>
         </span>
 
     </footer>
