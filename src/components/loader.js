b72d7b78b557e880ca90842b028faec1a096da17
Jacky Lee <aboutjax1990@gmail.com>
2017-12-05 22:13:31 +1300

    update loader to activity line animation

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LoadingSpinner = () => (
-  <div className='c-loading_spinner-wrap'>
-    <svg className='c-loading_spinner' width='60' height='20' viewBox='0 0 60 20' xmlns='http://www.w3.org/2000/svg'>
-      <circle cx='7' cy='15' r='4' />
-      <circle cx='30' cy='15' r='4' />
-      <circle cx='53' cy='15' r='4' />
-    </svg>
-  </div>
-)
+class LoadingSpinner extends Component {
+  componentDidMount(){
+    let path = document.querySelector(".c-loader__svg-stroke");
+
+    let pathLength = path.getTotalLength()
+
+    path.style.strokeDasharray = pathLength
+    path.style.strokeDashoffset = pathLength * 2
+  }
+
+  render(){
+    return(
+      <div className='c-loading_spinner-wrap'>
+        <svg width="34px" height="30px" viewBox="0 0 34 30" version="1.1">
+            <g stroke="#FFC236" fill="none" fillRule="evenodd" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <polyline className="c-loader__svg-stroke" points="2 15 8 15 12.5 1.5 21.5 28.5 26 15 32 15"></polyline>
+            </g>
+        </svg>
+      </div>
+    )
+  }
+}
 
 export default LoadingSpinner
