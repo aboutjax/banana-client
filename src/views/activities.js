ac3095830fa31bd5f7048508dd603fe06f65f8a2
Jacky Lee <aboutjax1990@gmail.com>
2019-02-19 10:50:25 +0800

    update year to date to 2019

--- src/views/activities.js
+++ src/views/activities.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import Activity from '../components/activity';
-import { Link } from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group';
-import LoadingSpinner from '../components/loader';
-import {getCookie} from '../components/cookieHelper'
-import fire from '../components/firebase'
-import _ from 'lodash';
-import {IconArrowRight, IconArrowLeft} from '../components/icons/icons'
+import React, { Component } from "react";
+import Activity from "../components/activity";
+import { Link } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group";
+import LoadingSpinner from "../components/loader";
+import { getCookie } from "../components/cookieHelper";
+import fire from "../components/firebase";
+import _ from "lodash";
+import { IconArrowRight, IconArrowLeft } from "../components/icons/icons";
 
 const activitiesPerPage = 30;
 
-class Activities extends Component{
+class Activities extends Component {
   constructor(props) {
     super(props);
 
@@ -19,135 +19,140 @@ class Activities extends Component{
       pager: {},
       loading: true,
       userUid: props.userUid
-    }
+    };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.setState({
       loading: true
-    })
+    });
   }
-
 
   // Check favourites state from Firebase
-  checkFavouriteStatus = () => {
-
-
-
-  }
+  checkFavouriteStatus = () => {};
 
   fetchData = () => {
-
-    let userAccessToken = getCookie('access_token');
-    let page = this.props.match.params.page || 1
-    let activitiesFetchUrl = 'https://www.strava.com/api/v3/athlete/activities?page='+ page
+    let userAccessToken = getCookie("access_token");
+    let page = this.props.match.params.page || 1;
+    let activitiesFetchUrl =
+      "https://www.strava.com/api/v3/athlete/activities?page=" + page;
 
     fetch(activitiesFetchUrl, {
-      method: 'get',
+      method: "get",
       headers: {
         "content-type": "application/json",
-        "authorization": "Bearer " + userAccessToken
-      }
-    }).then(function(response){
-      return response.json();
-    }).then( json => {
-      if(json.length < activitiesPerPage){
-        this.setState({
-          data: json,
-          loading: false,
-          pager: {
-            isLastPage: true,
-            isFirstPage: false,
-            nextPage: null,
-            currentPage: Number(this.props.match.params.page),
-            previousPage: Number(this.props.match.params.page) - 1
-          }
-        })
-      } else if(json.length === activitiesPerPage && this.props.match.params.page){
-        this.setState({
-          data: json,
-          loading: false,
-          pager: {
-            isLastPage: false,
-            isFirstPage: false,
-            nextPage: Number(this.props.match.params.page) + 1,
-            currentPage: Number(this.props.match.params.page),
-            previousPage: Number(this.props.match.params.page) - 1
-          }
-        })
-      } else {
-        this.setState({
-          data: json,
-          loading: false,
-          pager: {
-            isLastPage: false,
-            isFirstPage: true,
-            nextPage: 2,
-            currentPage: 1,
-            previousPage: null
-          }
-        })
+        authorization: "Bearer " + userAccessToken
       }
     })
-  }
+      .then(function(response) {
+        return response.json();
+      })
+      .then(json => {
+        if (json.length < activitiesPerPage) {
+          this.setState({
+            data: json,
+            loading: false,
+            pager: {
+              isLastPage: true,
+              isFirstPage: false,
+              nextPage: null,
+              currentPage: Number(this.props.match.params.page),
+              previousPage: Number(this.props.match.params.page) - 1
+            }
+          });
+        } else if (
+          json.length === activitiesPerPage &&
+          this.props.match.params.page
+        ) {
+          this.setState({
+            data: json,
+            loading: false,
+            pager: {
+              isLastPage: false,
+              isFirstPage: false,
+              nextPage: Number(this.props.match.params.page) + 1,
+              currentPage: Number(this.props.match.params.page),
+              previousPage: Number(this.props.match.params.page) - 1
+            }
+          });
+        } else {
+          this.setState({
+            data: json,
+            loading: false,
+            pager: {
+              isLastPage: false,
+              isFirstPage: true,
+              nextPage: 2,
+              currentPage: 1,
+              previousPage: null
+            }
+          });
+        }
+      });
+  };
 
   componentWillReceiveProps() {
+    let favouritesList = [];
+    let favouritesRef = fire
+      .database()
+      .ref("users/" + this.state.userUid + "/favourites");
 
-
-    let favouritesList = []
-    let favouritesRef = fire.database().ref('users/' + this.state.userUid + '/favourites');
-
-    favouritesRef.once('value', snapshot => {
+    favouritesRef.once("value", snapshot => {
       snapshot.forEach(child => {
-        let favouriteActivityId = child.child('activityId').val()
-        favouritesList.push(favouriteActivityId)
-      })
+        let favouriteActivityId = child.child("activityId").val();
+        favouritesList.push(favouriteActivityId);
+      });
 
       this.setState({
         loading: false,
         favouritesList: favouritesList
-      })
-    })
-
+      });
+    });
   }
 
   componentDidMount() {
-    this.fetchData()
+    this.fetchData();
   }
 
-  render(){
+  render() {
     const activitiesData = this.state.data;
     const activities = activitiesData.map((activity, index) => {
       let isFavourite;
 
-        if(this.state.favouritesList) {
-          isFavourite = _.includes(this.state.favouritesList, String(activity.id))
-        }
+      if (this.state.favouritesList) {
+        isFavourite = _.includes(
+          this.state.favouritesList,
+          String(activity.id)
+        );
+      }
 
-        return(
-            <Activity favourite={isFavourite} key={index} data={activity} mapDimension='400x400'/>
-        )
-    })
+      return (
+        <Activity
+          favourite={isFavourite}
+          key={index}
+          data={activity}
+          mapDimension="400x400"
+        />
+      );
+    });
 
-    if(this.state.loading){
-      return(
-
-          <div className="o-flex o-flex-align--center o-flex-justify--center">
-            <LoadingSpinner />
-          </div>
-
-      )
+    if (this.state.loading) {
+      return (
+        <div className="o-flex o-flex-align--center o-flex-justify--center">
+          <LoadingSpinner />
+        </div>
+      );
     } else {
       return (
         <div className="o-wrapper">
           <div>
             <Link to="/year-review">
               <div className="c-yrt-card t-top-spacing--xl">
-                <IconArrowRight className="c-yrt-card__right-arrow"/>
-                <h2 className="c-yrt-card__header">
-                  Your Year in Review
-                </h2>
-                <p className="c-yrt-card__description">Check your progress for 2018.</p>
+                <IconArrowRight className="c-yrt-card__right-arrow" />
+                <h2 className="c-yrt-card__header">Your Year in Review</h2>
+                <p className="c-yrt-card__description">
+                  Check your progress for 2019.
+                </p>
               </div>
             </Link>
             <div className="c-page-header">
@@ -158,29 +163,39 @@ class Activities extends Component{
               transitionAppear={true}
               transitionAppearTimeout={500}
               transitionEnterTimeout={500}
-              transitionLeaveTimeout={300}>
-              { activities }
+              transitionLeaveTimeout={300}
+            >
+              {activities}
             </CSSTransitionGroup>
-            <Paginator pager={this.state.pager}/>
+            <Paginator pager={this.state.pager} />
           </div>
         </div>
-      )
+      );
     }
   }
 }
 
 function Paginator(props) {
-  return(
+  return (
     <ul className="c-paginator">
       <li className="c-paginator__link t-right-spacing--xs">
-        <a className={ props.pager.currentPage === 1 ? 'disabled c-btn' : 'c-btn' } href={'/activities/page/' + props.pager.previousPage}><IconArrowLeft/></a>
+        <a
+          className={props.pager.currentPage === 1 ? "disabled c-btn" : "c-btn"}
+          href={"/activities/page/" + props.pager.previousPage}
+        >
+          <IconArrowLeft />
+        </a>
       </li>
       <li className="c-paginator__link t-left-spacing--xs">
-        <a className={ props.pager.isLastPage ? 'disabled c-btn' : 'c-btn' } href={'/activities/page/' + props.pager.nextPage}><IconArrowRight/></a>
+        <a
+          className={props.pager.isLastPage ? "disabled c-btn" : "c-btn"}
+          href={"/activities/page/" + props.pager.nextPage}
+        >
+          <IconArrowRight />
+        </a>
       </li>
     </ul>
-  )
+  );
 }
-
 
 export default Activities;
--- src/views/yearToDate.js
+++ src/views/yearToDate.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import LoadingSpinner from '../components/loader'
-import {getCookie} from '../components/cookieHelper'
-import CountUp, { startAnimation } from 'react-countup';
-import Waypoint from 'react-waypoint';
+import React, { Component } from "react";
+import _ from "lodash";
+import LoadingSpinner from "../components/loader";
+import { getCookie } from "../components/cookieHelper";
+import CountUp, { startAnimation } from "react-countup";
+import Waypoint from "react-waypoint";
 
 class YearToDate extends Component {
   constructor(props) {
@@ -16,146 +16,186 @@ class YearToDate extends Component {
       ytdRideTotals: {},
       ytdRunTotals: {},
       ytdSwimTotals: {}
-    }
+    };
   }
 
-  componentWillMount(){
+  componentWillMount() {
+    let userAccessToken = getCookie("access_token");
 
-    let userAccessToken = getCookie('access_token');
-
-    fetch('https://www.strava.com/api/v3/athlete', {
-      method: 'get',
+    fetch("https://www.strava.com/api/v3/athlete", {
+      method: "get",
       headers: {
         "content-type": "application/json",
-        "authorization": "Bearer " + userAccessToken
+        authorization: "Bearer " + userAccessToken
       }
-    }).then(function(response){
-      return response.json();
-    }).then( json => {
-
-      this.setState({
-        data: json
-      })
-    }).then( json => {
-
-      let totalsApiUrl = 'https://www.strava.com/api/v3/athletes/' +  this.state.data.id + '/stats';
-      fetch(totalsApiUrl, {
-        method: 'get',
-        headers: {
-          "content-type": "application/json",
-          "authorization": "Bearer " + userAccessToken
-        }
-      }).then(function(response){
-        return response.json();
-      }).then( json => {
-
-        this.setState({
-          ytdRideTotals: json.ytd_ride_totals,
-          ytdRunTotals: json.ytd_run_totals,
-          ytdSwimTotals: json.ytd_swim_totals,
-          loading: false
-        })
-      })
     })
-
+      .then(function(response) {
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          data: json
+        });
+      })
+      .then(json => {
+        let totalsApiUrl =
+          "https://www.strava.com/api/v3/athletes/" +
+          this.state.data.id +
+          "/stats";
+        fetch(totalsApiUrl, {
+          method: "get",
+          headers: {
+            "content-type": "application/json",
+            authorization: "Bearer " + userAccessToken
+          }
+        })
+          .then(function(response) {
+            return response.json();
+          })
+          .then(json => {
+            this.setState({
+              ytdRideTotals: json.ytd_ride_totals,
+              ytdRunTotals: json.ytd_run_totals,
+              ytdSwimTotals: json.ytd_swim_totals,
+              loading: false
+            });
+          });
+      });
   }
 
-  render(){
-    let ytdRideDistance = _.round(this.state.ytdRideTotals.distance / 1000 , 1)
-    let ytdRideElevationGain = _.round(this.state.ytdRideTotals.elevation_gain, 1)
-    let ytdRideMovingTime = _.round(this.state.ytdRideTotals.moving_time / 60 / 60, 1)
+  render() {
+    let ytdRideDistance = _.round(this.state.ytdRideTotals.distance / 1000, 1);
+    let ytdRideElevationGain = _.round(
+      this.state.ytdRideTotals.elevation_gain,
+      1
+    );
+    let ytdRideMovingTime = _.round(
+      this.state.ytdRideTotals.moving_time / 60 / 60,
+      1
+    );
 
-    let mountEverestUnit = _.round(this.state.ytdRideTotals.elevation_gain / 8848, 2)
+    let mountEverestUnit = _.round(
+      this.state.ytdRideTotals.elevation_gain / 8848,
+      2
+    );
 
-    if(!this.state.loading) {
+    if (!this.state.loading) {
       return (
         <div className="c-ytd">
           <div className="c-ytd__stats">
             <div className="c-ytd__section c-ytd__section--intro">
-
               <div className="c-ytd__intro">
-                <h1 className="c-ytd__intro__year">20<br/>18</h1>
-                <h1 className="c-ytd__intro__title">Year<br/>in<br/>Review</h1>
+                <h1 className="c-ytd__intro__year">
+                  20
+                  <br />
+                  19
+                </h1>
+                <h1 className="c-ytd__intro__title">
+                  Year
+                  <br />
+                  in
+                  <br />
+                  Review
+                </h1>
               </div>
 
               <ScrollHint />
             </div>
             <YearToDateStat
-              value={ytdRideDistance} unit='kilometers'
-              type='ride'
-              title='This year, you clocked'
+              value={ytdRideDistance}
+              unit="kilometers"
+              type="ride"
+              title="This year, you clocked"
             />
             <YearToDateStat
-              value={ytdRideElevationGain} unit='meters'
-              type='ride'
-              title='You climbed'
-              description={'That translates to climbing Mt Everest ' + mountEverestUnit + ' times!'}
+              value={ytdRideElevationGain}
+              unit="meters"
+              type="ride"
+              title="You climbed"
+              description={
+                "That translates to climbing Mt Everest " +
+                mountEverestUnit +
+                " times!"
+              }
             />
             <YearToDateStat
-              value={ytdRideMovingTime} unit='hours'
-              type='ride'
-              title='You sat in the saddle for'
+              value={ytdRideMovingTime}
+              unit="hours"
+              type="ride"
+              title="You sat in the saddle for"
             />
           </div>
         </div>
-      )
+      );
     } else {
-      return (
-        <LoadingSpinner />
-      )
+      return <LoadingSpinner />;
     }
-
-
   }
 }
 
 class YearToDateStat extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       value: 0,
       counted: false
-    }
+    };
   }
 
   _handleWaypointEnter = () => {
-    if(!this.state.counted){
-      startAnimation(this.myCountUp)
-      this.setState({value: this.props.value, counted: true})
+    if (!this.state.counted) {
+      startAnimation(this.myCountUp);
+      this.setState({ value: this.props.value, counted: true });
     }
-  }
+  };
 
-  render(){
-
-    return(
+  render() {
+    return (
       <div className={"c-ytd__section c-ytd__section--" + this.props.type}>
-        <Waypoint bottomOffset='20%' onEnter={this._handleWaypointEnter} />
+        <Waypoint bottomOffset="20%" onEnter={this._handleWaypointEnter} />
         <div className="c-ytd__stat">
           <h3 className="c-ytd__stat__title">{this.props.title}</h3>
           <h1 className="c-ytd__stat__value">
-            <CountUp separator="," start={0} duration={2} end={this.state.value} ref={(countUp) => {
-              this.myCountUp = countUp;
-            }}/>
+            <CountUp
+              separator=","
+              start={0}
+              duration={2}
+              end={this.state.value}
+              ref={countUp => {
+                this.myCountUp = countUp;
+              }}
+            />
           </h1>
           <h3 className="c-ytd__stat__unit">{this.props.unit}</h3>
-          {this.props.description ? <h3 className="c-ytd__stat__description">{this.props.description}</h3> : null}
+          {this.props.description ? (
+            <h3 className="c-ytd__stat__description">
+              {this.props.description}
+            </h3>
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
-
-function ScrollHint(){
-  return(
-    <svg className="c-ytd__scroll-hint" viewBox="0 0 539 65" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <g id="Page-2" stroke="none" fill="none" fill-rule="evenodd">
-            <path d="M144,32.2686567 C245,32.2686567 239.499145,2.70616862e-15 314.5,0 C389.500855,-3.60822483e-15 413,45.2089552 465.5,45.2089552 C518,45.2089552 539,48.5 539,56.6268657 C539,62.2089552 539,65 539,65 L-2.27373675e-13,65 C-2.27373675e-13,65 -2.27373675e-13,62.2089552 -2.27373675e-13,56.6268657 C-2.27373675e-13,36 43,32.2686567 144,32.2686567 Z" id="Rectangle" fill="#FFC236"></path>
-        </g>
+function ScrollHint() {
+  return (
+    <svg
+      className="c-ytd__scroll-hint"
+      viewBox="0 0 539 65"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Page-2" stroke="none" fill="none" fill-rule="evenodd">
+        <path
+          d="M144,32.2686567 C245,32.2686567 239.499145,2.70616862e-15 314.5,0 C389.500855,-3.60822483e-15 413,45.2089552 465.5,45.2089552 C518,45.2089552 539,48.5 539,56.6268657 C539,62.2089552 539,65 539,65 L-2.27373675e-13,65 C-2.27373675e-13,65 -2.27373675e-13,62.2089552 -2.27373675e-13,56.6268657 C-2.27373675e-13,36 43,32.2686567 144,32.2686567 Z"
+          id="Rectangle"
+          fill="#FFC236"
+        />
+      </g>
     </svg>
-  )
+  );
 }
-
 
 export default YearToDate;
